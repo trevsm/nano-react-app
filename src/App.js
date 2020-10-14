9330947de75bacb94ffc1ede10e9a26535b1e174
Trevor <trevorsm@pm.me>
2020-10-14 12:58:18 -0600

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'minireset.css'
+import Wrapper from './components/Wrapper'
+
+function App() {
+  return (
+    <Wrapper>
+      <h1>Nano React App</h1>
+      <p>- Hello world -</p>
+    </Wrapper>
+  );
+}
+
+export default App;
