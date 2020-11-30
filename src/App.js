381b098417b3694d68b0362484ff30de1288f7b4
unknown <trevorsm@pm.me>
2020-11-30 12:30:39 -0700

    removed jest and wrapper component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import 'minireset.css'
-import Wrapper from './components/Wrapper'
 
 function App() {
   return (
-    <Wrapper>
-      <h1>Nano React App</h1>
-      <p>- Hello world -</p>
-    </Wrapper>
+    <h1>Nano React App</h1>
   );
 }
 
